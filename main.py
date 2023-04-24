d9f906109c9cc2692454339ae87b83b886ead5f6
Leontii <krokxa228@gmail.com>
2023-04-24 23:14:49 +0500

    add func save_photo_to_yandex_disk

--- main.py
+++ main.py
@@ -14,5 +14,25 @@ def save_to_yandex_disk(token, url, file_name):
     response = requests.post("https://cloud-api.yandex.net/v1/disk/resources/upload", headers=headers, params=params)
     return response.json()
 
+def save_photo_to_yandex_disk(photo, token):
+    photo_sizes = photo["sizes"]
+    max_size = max(photo_sizes, key=lambda x: x['height'] * x['width'])
+    url = max_size["url"]
+    likes = photo["likes"]["count"]
+    file_name = f"{likes}.jpg"
+    response = save_to_yandex_disk(token, url, file_name)
+
+    if response.get("error"):
+        if response["error"].get["massage"] == "409 Conflict":
+            date = photo["date"]
+            file_name += f'_{date}'
+            response = save_to_yandex_disk(token, url, file_name)
+
+        print(f"Ошибка сохранения на Я.Диск: {response['error']}")
+    else:
+        print(f"Фото {file_name} сохранено на Я.Диске")
+
+    return {"file_name": file_name, "size": max_size["type"], "likes": likes}
+
 if __name__=='__main__':
     pprint(download_photos_from_vk(secret.id_vk, secret.TOKEN_vk))
